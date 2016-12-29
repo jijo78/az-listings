44c0c2e085ced0e73c34faffe6bec65ab203b1e9
Giuseppe de Marino <giuseppe.demarino@bbc.co.uk>
2016-12-29 15:51:28 +0000

    added pagination bootstrap

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,9 +2,9 @@
    'use strict';
 
   /**
-   * This is the TrainDeparture constructor
+   * This is the AzListing constructor
    * @constructor
-   * @this {TrainDeparture}
+   * @this {AzListing}
    */
     function AzListing() {
       var _this = this;
@@ -12,21 +12,36 @@
       //Global element.
       this.programmeLetters = document.querySelectorAll( '.listing__order' );
       this.queryResults = document.querySelector( '.output' );
+      this.queryResultsContainer = document.querySelector( '.output__items' );
 
 
       //Actions
       [].forEach.call( this.programmeLetters, function ( letter ) {
         letter.addEventListener( 'click', function(){
+            var value =  this.innerHTML,
+                url = '?letter='+ value;
 
-          //using the proxy url here, which it is set
-          //in app.js line 46.
-          var value = this.innerHTML,
-              url = '/listing?letter='+ value;
+          _this.getQuery( url, _this.dataSuccess.bind(_this) );
 
-              console.log(value);
-          _this.getQuery(url, _this.dataSuccess.bind(_this));
+          $('.listing__order').removeClass('active');
+          this.classList.add('active');
         });
-        } );
+      });
+
+      $('.output').on('click', '.pagination a', function( e ) {
+        e.preventDefault();
+
+        var value =  this.innerHTML,
+            pagination = document.querySelector('.pagination'),
+            dataLetter = pagination.dataset.letter,
+            url = '?letter='+ dataLetter+'&page='+ value,
+            target = e.target;
+
+        _this.getQuery( url, _this.dataSuccess.bind(_this) );
+        target.classList.remove('active');
+
+        e.currentTarget.classList.add('active');
+      });
 
     }
 
@@ -35,16 +50,15 @@
      * @param {[Function]} callback
      */
     AzListing.prototype.getQuery = function ( url, callback ) {
+
       var _this = this,
           deferred = $.Deferred();
 
-        if( this.searchValue ){
-          _this.searchValue.classList.add('spinner');
-        }
+          _this.queryResults.classList.add('spinner');
 
         $.ajax( {
             type: 'GET',
-            url: url,
+            url: '/listing'+ url,
             dataType:'json',
             success: onResponse,
             error: onError
@@ -54,8 +68,8 @@
          * Return a successful promise
          * @param {[Object]} response
          */
-        function onResponse(response) {
-            deferred.resolve(response);
+        function onResponse( response ) {
+            deferred.resolve( response );
             callback(response);
         }
 
@@ -63,10 +77,24 @@
          * Return an error on rejection
          * @param {[Object]} response
          */
-        function onError(response) {
-            deferred.reject(response);
+        function onError( response ) {
+            deferred.reject( response );
             callback((response.status +' '+ response.statusText));
         }
+
+        //function addClass(){
+        //   var elements = document.querySelectorAll( '.pagination a' ); // grab a reference to your element
+        //   [].forEach.call( elements, function ( element ) {
+        //     var elementIsClicked = false; // declare the variable that tracks the state
+        //     function clickHandler(){ // declare a function that updates the state
+        //       elementIsClicked = true;
+        //       console.log(elementIsClicked);
+        //       this.classList.add('active');
+        //     }
+        //     element.addEventListener( 'click', clickHandler);
+        //   });
+        // }
+
 
         return deferred.promise();
     };
@@ -80,7 +108,7 @@
     AzListing.prototype.dataLoop = function ( arr, callBackArg ,newArr ) {
       //safe check to make sure all arguments are passed in, and they are
       //of the right type.
-      if($.isArray(arr) &&
+      if($.isArray( arr ) &&
         callBackArg !== null &&
         $.isArray(newArr)
       ){
@@ -98,35 +126,46 @@
      */
     AzListing.prototype.dataSuccess = function ( data ) {
       //lets check we have some data back, and the data is the right format before we proceed.
-      if(!data && typeof data !== 'object'){
+      if( !data && typeof data !== 'object' ){
         throw new Error('Missing data.');
       }
 
       //lets store the feed in an array so we can let the view deal with the looping.
       var items = [],
           item,
+          pagNumber = [],
           _this =  this,
           template = Handlebars.compile( $( '#output-results' ).html() ),
           pagination = {
             page : data.atoz_programmes.page,
+            char : data.atoz_programmes.character,
             count : data.atoz_programmes.count,
             perPage : data.atoz_programmes.per_page,
-            totalPages : Math.round( data.atoz_programmes.count / data.atoz_programmes.per_page )
+            totalPages : Math.ceil( data.atoz_programmes.count / data.atoz_programmes.per_page ),
+            pages: pagNumber
           },
           context = {
             'programmes': items,
             'pagination':pagination
           };
-          console.log(pagination);
+
+      for( var i = 1; i <= pagination.totalPages; i++ ){
+        pagNumber.push(i);
+      }
+
+      //lets empty the html before a new call
+      _this.queryResults.innerHTML = '';
+
       //call to helpers
       _this.handlebarsHelpers();
 
-      if(data.atoz_programmes.elements){
-        _this.dataLoop( data.atoz_programmes.elements, item, items);
+      if( data.atoz_programmes.elements ){
+        _this.dataLoop( data.atoz_programmes.elements, item, items );
         var html = template( context );
         _this.queryResults.innerHTML = html;
+        _this.queryResults.classList.remove('spinner');
       }
-
+      
     };
 
     /**
@@ -137,20 +176,20 @@
 
       //equal deal with comparing values, which in Handlebars,
       //it is not possible with if statement.
-      Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+      Handlebars.registerHelper( 'ifCond', function( v1, v2, options ) {
         if(v1 > v2) {
           return options.fn(this);
         }
         return options.inverse(this);
       });
 
-      Handlebars.registerHelper('replace', function( find, replace, options) {
+      Handlebars.registerHelper( 'replace', function( find, replace, options ) {
       	var string = options.fn(this);
       	return string.replace( find, replace );
       });
 
     };
     //need to export the constructor to be able to unit test it.
-    exports.TrainDeparture = AzListing;
+    exports.AzListing = AzListing;
     return new AzListing();
 })(this);
